627d7ebb2bb62554e97c956c17ec02ca7305f77a
Hwang <10221092@gmail.com>
2024-07-07 05:59:10 +0700

    Update shop item

--- src/data/shop.ts
+++ src/data/shop.ts
@@ -12,13 +12,24 @@ export const ShopView: ShopItem[] = [
 	{
 		index: 202001,
 		itemId: 107009,
-		name: 'Fragile Resin',
+		name: 'Fragile Resin (Nhựa Dễ Vỡ)',
 		type: 'mora',
 		price: 10000000,
 		quantity: 1,
 		image: '<:FragileResin:1257820100325539861>',
 		description:
-			'An item used to restore Original Resin by 60 points.'
+			'An item used to restore Original Resin by 60 points (Vật phẩm được sử dụng để hồi phục 60 điểm Nhựa Nguyên Chất).'
+	},
+	{
+		index: 202002,
+		itemId: 104319,
+		name: 'Crown of Insight (Vương miện tri thức)',
+		type: 'mora',
+		price: 10000000,
+		quantity: 1,
+		image: '<:FragileResin:1257820100325539861>',
+		description:
+			'Character Talent Material used when leveling Combat Talents (Vật liệu Thiên Phú Nhân Vật được sử dụng khi nâng cấp Thiên Phú Chiến Đấu).'
 	},
 	{
 		index: 2,

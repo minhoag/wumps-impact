7207aecc02c640e4c0a51e630eae5ca39808a575
Hwang Jr <wandaandita2003@gmail.com>
2024-07-07 08:12:00 +0700

    Delete build directory

--- build/prisma/prisma-second.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _prisma_second_db_1 = require("@prisma-second-db");
-const prisma_second = new _prisma_second_db_1.PrismaClient();
-exports.default = prisma_second;

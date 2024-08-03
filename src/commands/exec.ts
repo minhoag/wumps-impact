90d4a822b0bc045f9a660ee8bd45144f64fefdaf
hoang <lql.euw@gmail.com>
2024-08-03 16:55:02 +0700

    Update exec

--- src/commands/exec.ts
+++ src/commands/exec.ts
@@ -8,7 +8,7 @@ const command: Command = {
 	cooldown: 10,
 	execute: async (message: Message<boolean>, args: string[]) => {
 		if (args.length < 1) return message.channel.send('Missing argruments');
-		const value: string = args[0];
+		const value: string = args[1];
 		let res = await fetch(`http://wumpus.site:12000/${value}`)
 			.then(async res => await res.json())
 		return message.channel.send('Execute complete. Response: ' + JSON.stringify(res));

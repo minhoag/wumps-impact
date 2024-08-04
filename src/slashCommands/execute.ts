63e33424cc9e7a681c18b542c87a56058c079316
hoang <lql.euw@gmail.com>
2024-08-05 03:18:35 +0700

    Fix bug execute

--- src/slashCommands/execute.ts
+++ src/slashCommands/execute.ts
@@ -51,13 +51,6 @@ const command: SlashCommand = {
 				if (confirmation.customId === 'cancel') {
 					replyEmbed.setTitle('Đã huỷ thành công').setDescription('Huỷ lệnh restart thành công!')
 				} else {
-					await fetch('https://backend.control.luxvps.net/v1/service/f1bfab30-b687-4f33-bd7e-10e6520cb79f/restart?token=11f66be3-e95b-4795-8434-abfdb181fa3f', {
-						method: 'POST',
-						headers: {
-							accept: 'application/json',
-							'Content-Type': 'application/json'
-						}
-					});
 					await fetch(
 						`http://wumpus.site:12000/restart`
 					).then(async res => {

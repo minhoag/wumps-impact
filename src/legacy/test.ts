74a2be42b73d61f2a5e397a2d543f1a4399397b0
root <wandaandita2003@gmail.com>
2025-01-25 09:47:39 +0700

    Upload

--- src/legacy/test.ts
+++ src/legacy/test.ts
@@ -8,16 +8,7 @@ const command: Command = {
   aliases: ['ts'],
   cooldown: 10,
   execute: async () => {
-    const res = await fetch(
-      `http://localhost:14861/api?cmd=1016&region=dev_gio&ticket=GM&uid=${1}`,
-    );
-    const item = await res.json();
-    // Materials are type 2
-    let all_item: any;
-    all_item = item.data.item_bin_data.pack_store.item_list.filter(
-      (i: any) => i.item_type === 2,
-    );
-    console.log(all_item);
+    return 'Test command';
   },
 };
 export default command;

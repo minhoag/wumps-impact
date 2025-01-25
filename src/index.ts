937d04408cd2c1a097134f235c5935473f1b1f6f
root <wandaandita2003@gmail.com>
2025-01-25 09:47:18 +0700

    Upload files

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,47 @@
-import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import dayjs from 'dayjs';
+import {
+  Client,
+  Collection,
+  DiscordAPIError,
+  GatewayIntentBits,
+} from 'discord.js';
 import { readdirSync } from 'fs';
 import { join } from 'path';
 
 import { Command, SlashCommand } from './types';
 
-process.on('uncaughtException', (error: any) => {
-  const stackLines = error.stack.split('\n');
-  if (error.message.includes("undefined (reading 'vi')")) {
-    const relevantLine = stackLines.find((line: string) =>
+process.on('uncaughtException', (error: unknown) => {
+  if (
+    error instanceof Error &&
+    error.message.includes("undefined (reading 'vi')")
+  ) {
+    const stackLines = error.stack?.split('\n');
+    const relevantLine = stackLines?.find((line: string) =>
       line.includes('E:\\Downloads\\Project\\wumps\\src\\slash'),
     );
     console.log('Uncaught Exception: Translation not found');
     console.log(relevantLine);
-  } else if (error.code === 'ENOTFOUND') {
-    console.log('No internet connection');
-  } else if (error.code === 'ECONNREFUSED') {
-    console.log('Connection refused');
+  } else if (error instanceof DiscordAPIError) {
+    if (error.code === 'ENOTFOUND') {
+      console.log('No internet connection');
+    } else if (error.code === 'ECONNREFUSED') {
+      console.log('Connection refused');
+    } else if (error.code === 10062) {
+      console.log(
+        'Unknown Interaction. Time stamp: ',
+        dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      );
+    } else if (error.code === 10008) {
+      console.log(
+        'Message is deleted. Time stamp: ',
+        dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      );
+    } else {
+      console.log('Uncaught Exception: Discord API Error');
+      console.log(error.message);
+    }
   } else {
-    console.error('Unhandled Rejection:', error);
+    console.error('Uncaught Exception: Unknown error: ', error);
   }
 });
 

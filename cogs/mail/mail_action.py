dda53ecf36a27acce5dbc1f66157631d1e8a436a
hoang.nguyen <nhoang120899@gmail.com>
2025-10-06 06:47:09 +0700

    bugfix: mail_view gzip encode

--- cogs/mail/mail_action.py
+++ cogs/mail/mail_action.py
@@ -215,7 +215,7 @@ class MailActions:
         title = mail_data['title'].strip()
         content = mail_data['content'].strip()
         expiry_days = 30
-        recipient = gzip.compress(recipient_list.encode('utf-8'))
+        recipient = gzip.compress(','.join(recipient_list).encode('utf-8'))
         recipient_count = len(recipient_list)
         log_message = f"{sender_discord_id}|{recipient}|{recipient_count}|{title}|{content}|{item_list}"
         log_id = create_log_record("MAIL", log_message)
